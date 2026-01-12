45fd74d5d866d38e1f7a4c21295be80668050e43
ljw <junwoo1519@softzion.com>
2026-01-12 14:16:43 +0900

    26/01/12 mod: 구조및 레이아웃 수정

---
+++ app/(public)/page.tsx
@@ -0,0 +1,15 @@
+const Login = () => (
+  <div className="flex w-full max-w-md flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <form className="flex flex-col w-full space-y-2">
+      <label htmlFor="id">Id</label>
+      <input type="id" id="id" name="id" required className="border border-zinc-300 dark:border-zinc-700"/>
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" name="password" required
+        className="border border-zinc-300 dark:border-zinc-700"/>
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login
+      </button>
+    </form>
+  </div>
+);
+
+export default Login;
