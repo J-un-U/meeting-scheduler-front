95e6b7408b2e6e822e233e3a61049d96842d6031
ljw <junwoo1519@softzion.com>
2026-01-20 14:05:07 +0900

    26/01/20 mod: 백엔드 통신 테스트 성공

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -8,15 +8,30 @@ const Login = () => {
     router.push('/dashboard');
   }
 
+  const handleTest = () => {
+    fetch('api/meetings', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: '회의',
+        description: '설명',
+        meetingType: 'SINGLE_DATE',
+        maxParticipants: 5,
+      }),
+    }).then(res => res.json()).then(data => console.log(data));
+  }
+
   return (
     <div className="flex w-full max-w-md flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <form className="flex flex-col w-full space-y-2" action={handleSignIn}>
+      <form className="flex flex-col w-full space-y-2" action={handleTest}>
         <label htmlFor="id">Id</label>
         <input type="text" id="id" name="id" required className="border border-zinc-300 dark:border-zinc-700"/>
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" required
           className="border border-zinc-300 dark:border-zinc-700"/>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login
         </button>
       </form>
     </div>
