45d36e61369ff03502eca9763d7aeae43782116e
ljw <junwoo1519@softzion.com>
2026-02-02 11:20:15 +0900

    26/02/02 mod: 약속 만들기 폼 추가

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -5,6 +5,9 @@ import Header from "@/app/components/layout/Header";
 import Sidebar from "@/app/components/layout/Sidebar";
 import Content from "@/app/components/layout/Content";
 import Modal from "@/app/components/Modal";
+import { Field, Fieldset, Input, Label, Select, Textarea } from "@headlessui/react";
+import clsx from "clsx";
+import { DatePicker } from "@/app/components/date-picker/DatePicker";
 
 const Dashboard = () => {
   const [isCreateMeetingOpen, setIsCreateMeetingOpen] = useState(false);
@@ -41,12 +44,52 @@ const Dashboard = () => {
         footer={<button className="rounded-md bg-blue-600 px-3 py-1.5 text-sm text-white" onClick={closeCreateMeeting}>확인</button>}
       >
         <form className={"flex flex-col gap-2"}>
-          <label htmlFor="title">약속 제목</label>
-          <input id={"title"} type="text"/>
-          <label htmlFor="description">약속 설명</label>
-          <textarea id={"description"}/>
-          <label htmlFor="title">최대 인원</label>
-          <select id={"maxParticipants"}></select>
+          <Fieldset>
+            <Field>
+              <Label className="text-sm/6 font-medium text-black">약속 제목</Label>
+              <Input
+                className={clsx(
+                  'mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-black',
+                  'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+                )}
+              />
+            </Field>
+            <Field>
+              <Label className="text-sm/6 font-medium text-black">약속 설명</Label>
+              <Textarea
+                className={clsx(
+                  'mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-black',
+                  'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+                )}
+              />
+            </Field>
+            <Field>
+              <Label className="text-sm/6 font-medium text-black">달 선택</Label>
+              <DatePicker mode={"month"}/>
+            </Field>
+            <Field>
+              <Label className="text-sm/6 font-medium text-black">최대 인원</Label>
+              <Select
+                className={clsx(
+                  'mt-3 block w-full appearance-none rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-black',
+                  'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25',
+                  // Make the text of each option black on Windows
+                  '*:text-black'
+                )}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </Select>
+            </Field>
+          </Fieldset>
         </form>
       </Modal>
     </>
